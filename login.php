c03d15f795c1c11529b095ac4565d7c83e3de17c
Monochromas <73582380+Monochromas@users.noreply.github.com>
2024-04-12 01:23:35 +0800

    logintest2

--- login.php
+++ login.php
@@ -1,31 +1,3 @@
-
-Here's an example HTML form for the username and password:
-
-html
-Copy code
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br><br>
-        
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
-        
-        <input type="submit" value="Login">
-    </form>
-</body>
-</html>
-And here's an example PHP script (login.php) to handle the login process:
-
-php
-Copy code
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
